58149c2ddcecc96a7076628c7683e47033824d5a
gouri <mahangadegouri@gmail.com>
2021-05-26 17:04:31 +0530

    A change has been added

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 
 from django.urls import path, include
 from django.contrib import admin
-
 from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,5 @@ urlpatterns = [
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
